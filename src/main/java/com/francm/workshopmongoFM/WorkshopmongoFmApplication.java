945fa23a5821ccb969b7b9b55a5b5fba8498904d
francmagalham <francmagalham.gmail>
2018-10-27 17:02:55 +0100

    Commit 1 - projeto criado

---
+++ src/main/java/com/francm/workshopmongoFM/WorkshopmongoFmApplication.java
@@ -0,0 +1,12 @@
+package com.francm.workshopmongoFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoFmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoFmApplication.class, args);
+	}
+}
